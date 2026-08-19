e0112826e4287a39ce6ccbd1edacd4521423a3fe
pobingbing <leedmswls@gmail.com>
2026-08-19 16:23:35 +0900

    Member (#5)
    * MemberController 수정
    
    * MemberCotroller 스쿼시 테스트 print1()
    
    * MemberCotroller 스쿼시 테스트 print2()
    
    * MemberCotroller 스쿼시 테스트 print3()

--- src/mvc/controller/MemberController.java
+++ src/mvc/controller/MemberController.java
@@ -4,4 +4,16 @@ public class MemberController {
 	public void print() {
 		System.out.println("member 브랜치에서 수정");
 	}
+  
+	public void print2() {
+		System.out.println("스쿼시 테스트1");
+	}
+	
+	public void print3() {
+		System.out.println("스쿼시 테스트2");
+	}
+	
+	public void print4() {
+		System.out.println("스쿼시 테스트3");
+	}
 }
